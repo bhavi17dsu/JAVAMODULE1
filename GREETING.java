e7de98fef986e3edabae78810454824da201d344
bhavi17dsu <allaparthibhavitha@gmail.com>
2025-08-21 20:05:02 +0530

    Update and rename GREETING to GREETING.java

---
+++ GREETING.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class greeting{
+public static void main(String []args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter name: ");
+String name=sc.nextLine();
+System.out.println("enter age:");
+int age=sc.nextInt();
+System.out.println("hello "+ name +" is your age really "+age);
+}
+}
