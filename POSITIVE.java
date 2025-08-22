4cb7d76b9b1630db6e49405dcf15a8ec1309e45e
bhavi17dsu <allaparthibhavitha@gmail.com>
2025-08-22 13:44:23 +0530

    Update and rename POSITIVE to POSITIVE.java

---
+++ POSITIVE.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Positive{
+    public static void main(String []args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the number:");
+    int a=sc.nextInt();
+    if(a>0){
+        System.out.println("the number is positive");
+    }
+    else{
+        System.out.println("the number is negative");
+    }
+}
+}
