12153c355c7fe16abc5ec9b1db52dadf3a2e4ed7
bhavi17dsu <allaparthibhavitha@gmail.com>
2025-08-26 11:47:00 +0530

    Create searcharray.java

---
+++ searcharray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class array{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int n[]=new int[5];
+        System.out.println("enter numbers:");
+        for(int i=0;i<n.length;i++){
+            n[i]=sc.nextInt();
+        }
+        System.out.println("ENTER THE ELEMENT TO SEARCH: ");
+        int search=sc.nextInt();
+        boolean found=false;
+        for(int i=0;i<n.length;i++){
+            if(n[i] == search){
+                System.out.println("element "+search+" found at position: "+(i));
+                found=true;
+                break;
+            }
+        }
+        if(!found){
+        System.out.println("element "+search+" not found");
+        }
+    }
+}
